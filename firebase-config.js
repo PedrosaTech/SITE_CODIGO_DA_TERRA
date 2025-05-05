e00135b76362ab28494bcfcf1166edd4dab35d5b
Júlio Cesar Alves Pedrosa <pedrosa.julio@gmail.com>
2025-05-04 21:57:13 -0300

    CONFIGURAÇÃO FIREBASECONFIG

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
-// public/firebase-config.js
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyDCMze76XmCGqufSMGO7NOZS8ad4_HROzA",
   authDomain: "codigodaterra-7c22e.firebaseapp.com",
+  databaseURL: "https://codigodaterra-7c22e-default-rtdb.firebaseio.com",
   projectId: "codigodaterra-7c22e",
   storageBucket: "codigodaterra-7c22e.firebasestorage.app",
   messagingSenderId: "32808222509",
   appId: "1:32808222509:web:f6a7e8ecd1c1ec2a20dda4",
+  measurementId: "G-XLHCVTSPJS"
 };
-
-firebase.initializeApp(firebaseConfig);
--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,11 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "codigodaterra-7c22e.firebaseapp.com",
-  projectId: "codigodaterra-7c22e",
-  storageBucket: "codigodaterra-7c22e.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
-};
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyDCMze76XmCGqufSMGO7NOZS8ad4_HROzA",
+    authDomain: "codigodaterra-7c22e.firebaseapp.com",
+    databaseURL: "https://codigodaterra-7c22e-default-rtdb.firebaseio.com",
+    projectId: "codigodaterra-7c22e",
+    storageBucket: "codigodaterra-7c22e.firebasestorage.app",
+    messagingSenderId: "32808222509",
+    appId: "1:32808222509:web:f6a7e8ecd1c1ec2a20dda4",
+    measurementId: "G-XLHCVTSPJS"
+  };
