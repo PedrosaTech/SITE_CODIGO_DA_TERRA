a1f0c26b6b40c382edd063760c9a9f8b5745dc7b
Júlio Cesar Alves Pedrosa <pedrosa.julio@gmail.com>
2025-05-13 17:15:43 -0300

    ajustefirebase painel-admin

--- public/admin/painel-admin.js
+++ public/admin/painel-admin.js
@@ -1,3 +1,16 @@
+// 🔌 Configuração e inicialização do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDCMze76XmCGqufSMGO7NOZS8ad4_HROzA",
+  authDomain: "codigodaterra-7c22e.firebaseapp.com",
+  databaseURL: "https://codigodaterra-7c22e-default-rtdb.firebaseio.com",
+  projectId: "codigodaterra-7c22e",
+  storageBucket: "codigodaterra-7c22e.firebasestorage.app",
+  messagingSenderId: "32808222509",
+  appId: "1:32808222509:web:f6a7e8ecd1c1ec2a20dda4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 document.addEventListener("DOMContentLoaded", () => {
   const emailAutorizado = "pedrosa.julio@gmail.com";
 
