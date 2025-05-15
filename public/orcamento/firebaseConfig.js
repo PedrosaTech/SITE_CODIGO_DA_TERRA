c9b180baca1d11c422ff32021e2e577d2caf4842
Júlio Cesar Alves Pedrosa <pedrosa.julio@gmail.com>
2025-05-15 15:41:18 -0300

    Força inclusão da pasta orcamento no deploy

---
+++ public/orcamento/firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCMze76XmCGqufSMGO7NOZS8ad4_HROzA",
+  authDomain: "codigodaterra-7c22e.firebaseapp.com",
+  databaseURL: "https://codigodaterra-7c22e-default-rtdb.firebaseio.com",
+  projectId: "codigodaterra-7c22e",
+  storageBucket: "codigodaterra-7c22e.firebasestorage.app",
+  messagingSenderId: "32808222509",
+  appId: "1:32808222509:web:f6a7e8ecd1c1ec2a20dda4",
+  measurementId: "G-XLHCVTSPJS"
+};
+firebase.initializeApp(firebaseConfig);
